52bc3cb719547f9a5a267616f0887b9b5dc71eca
smartfridgee <70762397+smartfridgee@users.noreply.github.com>
2021-07-17 18:13:50 +0200

    version 1.0 BETA
    Start of the project.

---
+++ database.js
@@ -0,0 +1,110 @@
+const database = [
+    {
+        img: "https://ok-kolej.pl/data/media/28/_MG_9850.jpg",
+        city: "Szczecin",
+        street: "Wolińska",
+        tag: "szczecin;wolińska;wolinska;d29-401;",
+        lat: "53.409005",
+        long: "14.704273",
+        author: "Kacper Marciszewski"
+    },
+
+    // {
+    //     img: "https://ok-kolej.pl/data/media/22/DSC_7805.jpg",
+    //     city: "Rzepin",
+    //     street: "Obwodnica",
+    //     tag: "rzepin;obwodnica",
+    //     lat: "",
+    //     long: ""
+    // },
+
+    {
+        img: "https://ok-kolej.pl/data/media/196/EN63H-001_2.jpg",
+        city: "Wojcieszyn",
+        street: "Armii Krajowej",
+        tag: "wojcieszyn;nowogard;d29-402",
+        lat: "53.685049",
+        long: "15.136591",
+        author: "Mikołaj Paśko"
+    },
+
+    {
+        img: "https://ok-kolej.pl/data/media/22/_MG_9737.jpg",
+        city: "Szczecin",
+        street: "Chocimska",
+        tag: "szczecin;chocimska;przeruchana;podjuchy;odrzanka;d29-273;klucz",
+        lat: "53.350736",
+        long: "14.570149",
+        author: "Kacper Marciszewski"
+    },
+
+    {
+        img: "https://ok-kolej.pl/data/media/37/EN57AL-1522.jpg",
+        city: "Dębogóra",
+        street: "Jezioro",
+        tag: "dębogóra;jezioro;odrzanka;d29-273",
+        lat: "53.149902",
+        long: "14.451950",
+        author: "Mikołaj Paśko"
+    },
+
+    {
+        img: "https://ok-kolej.pl/data/media/28/EU07-437__EU07-062.jpg",
+        city: "Szczecin",
+        street: "Załom",
+        tag: "szczecin;zalom;d29-401;kasztanowe",
+        lat: "53.435440",
+        long: "14.753579",
+        author: "Mikołaj Paśko"
+    },
+
+    {
+        img: "https://live.staticflickr.com/65535/49739790347_c489417c9e_k.jpg",
+        city: "Rzepin",
+        street: "Wiadukt",
+        tag: "rzepin;wiadukt;d29-3;e20;gajec",
+        lat: "52.348472",
+        long: "14.781421",
+        author: "Bastian Wierzchnicki"
+    },
+
+    {
+        img: "https://live.staticflickr.com/65535/50986312938_0eab67a2bc_k.jpg",
+        city: "Mostki",
+        street: "Wiadukt",
+        tag: "mostki;wiadukt;d29-3;e20;kasztanowe;dk92",
+        lat: "52.274084",
+        long: "15.376679",
+        author: "Kacper Marciszewski"
+    },
+
+    {
+        img: "https://live.staticflickr.com/65535/49704651993_50a83e3c70_k.jpg",
+        city: "Szczecin",
+        street: "Tczewska",
+        tag: "szczecin;dabie;dąbie;tczewska;d29-401",
+        lat: "53.397311",
+        long: "14.688819",
+        author: "Bastian Wierzchnicki"
+    },
+
+    {
+        img: "https://live.staticflickr.com/65535/49222959346_fe6d524b37_k.jpg",
+        city: "Czepino",
+        street: "Kolejowa",
+        tag: "czepno;kolejowa;d29-273;odrzanka;d29-401",
+        lat: "53.284875",
+        long: "14.512674",
+        author: "Bastian Wierzchnicki"
+    },
+
+    {
+        img: "https://live.staticflickr.com/65535/50071067548_72be1df53a_k.jpg",
+        city: "Szczecin",
+        street: "Krasińskiego",
+        tag: "szczecin;niebuszewo,krasinskiego;police",
+        lat: "53.450006",
+        long: "14.540924",
+        author: "Kacper Marcieszewski"
+    }
+]
---
+++ script.js
@@ -0,0 +1,120 @@
+const contentSection = document.getElementById('group');
+const buttonContainer = document.querySelector('.button-container');
+
+let dataCounter = 0;
+
+function loadData(){
+    for(i = 0; i < 6; i++){
+        city = database[dataCounter].city;
+        street = database[dataCounter].street;
+        img = database[dataCounter].img;
+        
+        let element = document.createElement('div');
+        element.classList.add('element');
+        element.setAttribute("id", dataCounter);
+        element.setAttribute("onclick", "loadDetails(event)");
+        element.innerHTML = '<img src='+img+'><div class="text-container"><h2 class="title">'+city+', '+street+'</h2><p class="more">Więcej informacji</p></div>';
+
+        contentSection.appendChild(element); 
+        
+        dataCounter++;
+
+        if(dataCounter == database.length){
+            buttonContainer.innerHTML = "-koniec-";
+            return;
+        }
+    }
+    buttonContainer.innerHTML = '<button id="load" class="load" onclick="loadButton()">Załaduj Więcej</button>';
+}
+
+function loadButton(){
+    loadData();
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+let serachInput = document.getElementById('search');
+serachInput.addEventListener('keyup', ()=>{
+    searchLoad();
+})
+
+function searchLoad(){
+    searchValue = serachInput.value;
+    contentSection.innerHTML = "";
+
+    if(searchValue == "") return dataCounter = 0, loadData();
+    
+    let results = [];
+
+    database.forEach(item => {
+        if(item.tag.includes(searchValue.toLowerCase()) == true) return results.push(item);
+        if(item.city.includes(searchValue) == true) return results.push(item);
+        if(item.street.includes(searchValue) == true) return results.push(item);
+    })
+
+    results.forEach(item => {
+        city = item.city;
+        street = item.street;
+        img = item.img;
+        
+        let element = document.createElement('div');
+        element.classList.add('element');
+        element.setAttribute("id", database.indexOf(item));
+        element.setAttribute("onclick", "loadDetails(event)");
+        element.innerHTML = '<img src='+img+'><div class="text-container"><h2 class="title">'+city+', '+street+'</h2><p class="more">Więcej informacji</p></div>';
+
+        contentSection.appendChild(element); 
+    })
+
+    buttonContainer.innerHTML = "-koniec-";
+
+}
+
+const detailsElement = document.querySelector('.details-container');
+
+const detailsCityEL = document.getElementById('city');
+const detailsStreetEL = document.getElementById('street');
+const detailsImgEL = document.getElementById('image');
+const detailsAuthorEL = document.getElementById('author');
+
+function loadDetails(event){
+    detailsId = event.currentTarget.id;
+
+    detailsCity = database[detailsId].city;
+    detailsStreet = database[detailsId].street;
+    detailsImg = database[detailsId].img;
+    detailsLat = database[detailsId].lat;
+    detailsLong = database[detailsId].long;
+    detailsAuthor = database[detailsId].author;
+
+    detailsCityEL.innerHTML = detailsCity;
+    detailsStreetEL.innerHTML = detailsStreet;
+    detailsAuthorEL.innerHTML = detailsAuthor;
+    detailsImgEL.setAttribute("src", detailsImg);
+
+    
+
+    locationMap.setView([detailsLat, detailsLong], 13);
+    marker.setLatLng([detailsLat, detailsLong]);
+
+    detailsElement.classList.remove('hidden');
+}
+
+let closeTrigger = document.getElementById('closeTrigger');
+closeTrigger.onclick = closeDetails;
+
+function closeDetails(){
+    detailsElement.classList.add('hidden');
+}
+
+window.onload = loadData();
+
+/////////////////////////MAP\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var locationMap = L.map('locationMap').setView([0, 0], 13);
+const marker = L.marker([0,0]).addTo(locationMap);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(locationMap);
